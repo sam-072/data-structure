a5c52321ce08bba55e2078f4c7ceaee34db813dc
sam-072 <shyamprakash807@gmail.com>
2021-08-30 19:40:08 +0530

    longest consecutive sequence

---
+++ hash map/longest consecutive sequence.py
@@ -0,0 +1,30 @@
+def longestsequence(a):
+    length=0
+    startpoint=0
+    d=dict()
+    for i in a:
+        d[i]=d.get(i,True)
+    for i in d:
+        if d[i]:
+            c=1
+            j=i+1
+            while d.get(j,False):
+                d[j]=False
+                c+=1
+                j+=1
+            j=i-1
+            while d.get(j,False):
+                c+=1
+                d[j]=False
+                j-=1
+            if c>length:
+                length=c
+                startpoint=j+1
+            # print(length,c)
+    return startpoint,startpoint+length-1
+
+if __name__=='__main__':
+    a=list(map(int, input().split()))
+    print(longestsequence(a))
+
+             
