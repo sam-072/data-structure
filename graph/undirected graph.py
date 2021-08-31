09698365544204fa9734977ded985f02f03ac688
sam-072 <shyamprakash807@gmail.com>
2021-08-31 18:49:04 +0530

    dfs and bfs

--- graph/undirected graph.py
+++ graph/undirected graph.py
@@ -1,3 +1,4 @@
+import queue
 class Graph:
     def __init__(self,nVertices):
         self.nVertices=nVertices
@@ -20,12 +21,42 @@ class Graph:
     def __str__(self):
         return str(self.adjMatrix)
 
+    def dfs(self):
+        visited=[False for i in range(self.nVertices)]
+        self.__dfsHelper(0,visited)
+    
+    def __dfsHelper(self,sv,visited):
+        print(sv)
+        visited[sv]=True
+        for i in range(self.nVertices):
+            if self.adjMatrix[sv][i]>0 and visited[i] is False:
+                self.__dfsHelper(i, visited)
+
+    def bfs(self):
+        q=queue.Queue()
+        q.put(0)
+        visited=[False for i in range(self.nVertices)]
+        visited[0]=True
+        while q.empty() is False:
+            u=q.get()
+            print(u)
+            for i in range(self.nVertices):
+                if self.adjMatrix[u][i]>0 and visited[i] is False:
+                    q.put(i)
+                    visited[i]=True
+
+
+
 if __name__=='__main__':
     g=Graph(5)
-    g.addEdge(1, 3)
-    g.addEdge(2, 3)
-    g.addEdge(1, 2)
-    g.removeEdge(1, 2)
+    g.addEdge(0,1)
+    g.addEdge(1,3)
+    g.addEdge(0,2)
+    g.addEdge(2,3)
+    g.addEdge(2,4)
     print(g)
+    g.dfs()
+    print()
+    g.bfs()
     print(g.containEdge(3, 2))
     print(g.containEdge(3, 3))
