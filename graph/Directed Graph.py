427808b931b99a70617dcddad11f7d2dfd9c1a30
sam-072 <shyamprakash807@gmail.com>
2021-09-07 18:06:46 +0530

    directed Graph using adjcent Matrix

--- graph/Directed Graph.py
+++ graph/Directed Graph.py
@@ -6,9 +6,9 @@ class Graph:
         self.adjMatrix=[[0 for i in range(nVertices)] for j in range(nVertices)]
         # the adjgency matrix will contains the vertices from 0 to nVertices - 1
 
-    def addEdge(self,v1,v2):
-        self.adjMatrix[v1][v2]=1
-        self.adjMatrix[v2][v1]=1
+    def addEdge(self,v1,v2,wt):
+        self.adjMatrix[v1][v2]=wt
+        self.adjMatrix[v2][v1]=wt
     
     def removeEdge(self,v1,v2):
         if self.containEdge(v1,v2) is False:
@@ -75,40 +75,29 @@ class Graph:
 
         return False
 
-    def getPath(self,v1,v2):
-        visited=[False for i in range(self.nVertices)]
+    def __minDist(self,distance,visited):
+        mindis=sys.maxsize
+        minindex=-1
         for i in range(self.nVertices):
-            if visited[i] is False:
-                path=self.__getPathHelper(v1,v2,visited)
-                if path != None:
-                    return path
-        return None
+            if visited[i] is False and (minindex==-1 or distance[minindex]> distance[i]):
+                minindex=i
+        return minindex
     
-    def __getPathHelper(self,v1,v2,visited):
-        q=queue.Queue()
-        q.put(v1)
-        d=dict()
-        z=0
-        visited[v1]=True
-        while q.empty() is False:
-            front=q.get()
-            for i in range(self.nVertices):
-                if self.adjMatrix[front][i]>0 and visited[i] is False:
-                    d[i]=d.get(i,front)
-                    q.put(i)
-                    visited[i]=True
-                    if i==v2:
-                        z=-1
-                        break
-            if z==-1:
-                break
-        if z==-1:
-            l=[v2]
-            while d.get(l[-1],-1)!=-1:
-                l.append(d[l[-1]])
-            return l
-        else:
-            return None
+    def getPath(self):
+        visited=self.nVertices*[False]
+        distance=self.nVertices*[sys.maxsize]
+        distance[0]=0
+        for i in range(self.nVertices-1):
+            u=self.__minDist(distance,visited)
+            visited[u]=True
+            for j in range(self.nVertices):
+                if self.adjMatrix[u][j]>0 and visited[j] is False:
+                    currD=distance[u]+ self.adjMatrix[u][j]
+                    if distance[j]> currD:
+                        distance[j]=currD
+
+        for i in range(self.nVertices):
+            print(i,distance[i])
     
     def isConnected(self):
         visited=[False for i in range(self.nVertices)]
@@ -151,19 +140,10 @@ class Graph:
     
 
 if __name__=='__main__':
-    g=Graph(7)
-    g.addEdge(0,1)
-    g.addEdge(0,3)
-    g.addEdge(2,4)
-    g.addEdge(2,5)
-    g.addEdge(5,6)
-    # print(g)
-    # g.dfs()
-    # print()
-    # g.bfs()
-    # print(g.containEdge(3, 2))
-    # print(g.containEdge(0, 6))
-    # print(g.hasPath(3, 1))
-    # print(g.getPath(2, 6))
-    print(g.isConnected())
-    print(g.ConnectedComponents())
+    v,e=map(int, input().split())
+    g=Graph(v)
+    for i in range(e):
+        v1,v2,wt=map(int, input().split())
+        g.addEdge(v1, v2, wt)
+    g.getPath()
+    
