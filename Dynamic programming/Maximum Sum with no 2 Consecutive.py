7189db6923a606408fa5a543ce6126fb114f56f1
sam-072 <shyamprakash807@gmail.com>
2021-10-25 11:59:01 +0530

    Maximum Sum with no 2 Conecutive solution using DP in python

---
+++ Dynamic programming/Maximum Sum with no 2 Consecutive.py
@@ -0,0 +1,30 @@
+# Code by : Sam._.072
+
+# ------RECURSIVE APPROACH------
+# Time Complexity : O(2^N)
+
+def maxSum_R(arr, n):
+    if n == 1:
+        return arr[0]
+    elif n == 2:
+        return max(arr[0], arr[1])
+    return max(maxSum_R(arr,n-1), maxSum_R(arr,n-2)+arr[n-1])
+
+# -------ITERATIVE APPROACH-------
+# Time Complexity : O(N)
+#  Space Complexity : O(N)
+
+def maxSum(arr):
+    n=len(arr)
+    dp = [0 for i in range(n+1)]
+    dp[1] = arr[1]
+    dp[2] = max(arr[0], arr[1])
+    for i in range(3,n+1):
+        dp[i] = max(dp[i-1], dp[i-2]+arr[i-1])
+    return dp[n]
+
+if __name__=='__main__':
+    n=int(input())
+    arr=list(map(int, input().split()))
+    print(maxSum_R(arr, n))
+    print(maxSum(arr))
